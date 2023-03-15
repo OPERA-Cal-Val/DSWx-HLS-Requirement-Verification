c8c36f883d399e79fab84ceb1168de6b68c42f5a
cmarshak <charles.marshak@gmail.com>
2023-03-15 15:33:44 -0700

    raise errors for confidence thresholds

--- sample_runs.zsh
+++ sample_runs.zsh
@@ -1,3 +1,3 @@
-python verify_all.py --confidence_minimum 70 --class_selection_from val --downsample_to_3_ha False
-# python verify_all.py --confidence_minimum 70 --class_selection_from dswx --downsample_to_3_ha False
-# python verify_all.py --confidence_minimum 70 --class_selection_from val --downsample_to_3_ha True
+python verify_all.py --class_selection_from val --downsample_to_3_ha False
+# python verify_all.py --class_selection_from dswx --downsample_to_3_ha False
+# python verify_all.py --class_selection_from val --downsample_to_3_ha True
